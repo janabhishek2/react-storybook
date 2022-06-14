1c9a46728d063662f2512d699149c7a775bf4100
Abhishek Jan <abhishek.jan@sendinblue.com>
2022-06-14 12:33:28 +0530

    Init

--- app1/src/components/Button/Button.stories.js
+++ app1/src/components/Button/Button.stories.js
@@ -4,6 +4,9 @@ import React from "react";
 export default {
   title: "Form/Control/Button",
   component: Button,
+  args:{
+      children:"Default String"
+  }
 };
 
 export const Primary = () => <Button variant="primary">Primary String</Button>;
@@ -13,29 +16,4 @@ export const Secondary = () => (
 export const Success = () => <Button variant="success">Success String</Button>;
 export const Danger = () => <Button variant="danger">Danger String</Button>;
 
-const Template = (args) => <Button {...args} />;
 
-export const PrimaryArgs = Template.bind({});
-
-PrimaryArgs.args = {
-  variant: "primary",
-  children: "Custom String",
-};
-
-export const LongPrimaryA = Template.bind({});
-
-LongPrimaryA.args = {
-  ...PrimaryArgs.args,
-  children: "Long primary args .. ",
-};
-
-LongPrimaryA.storyName = "Args story - Long Primary Args";
-PrimaryArgs.storyName = "Args Story - Primary";
-export const SecondaryArgs = Template.bind({});
-
-SecondaryArgs.args = {
-  variant: "secondary",
-  children: "Secondary Custom String",
-};
-
-SecondaryArgs.storyName = "Args story - Secondary";
